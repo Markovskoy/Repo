f4e444c0a56d1da5e1629aa0409c8de316c870a0
Markovskoy <v_markovskoy@mail.ru>
2025-06-19 23:38:30 +0300

    test: добавил получения hostname

--- work_scripts/python_scripts/перевыпуск_ssl_сертов/main.py
+++ work_scripts/python_scripts/перевыпуск_ssl_сертов/main.py
@@ -31,12 +31,58 @@ if missing:
     sys.exit(1)
 
 def load_servers(filepath):
-    with open(filepath, encoding="uf-8") as f:
+    with open(filepath, encoding="utf-8") as f:
         data = yaml.safe_load(f)
     if "servers" not in data:
         print("В файле нет ключа 'servers'")
         sys.exit(1)
     return data['servers']
 
-servers = load_servers('./servers/servers.yaml')
-print(servers)
+def get_hostname(ip, port, username, password):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        client.connect(hostname=ip, port=port, username=username, password=password, timeout=10)
+        stdin, stdout, stderr = client.exec_command("hostname -f")
+        output = stdout.read().decode().strip()
+        client.close()
+        return output
+    except Exception as e:
+        print(f"[Ошибка] не удалось подключиться к {ip}:{port} - {e}")
+        return None
+
+
+def menu():
+    print("""
+Выберите действие:
+1. Сгенерировать файл запроса сертификата
+2. Применить подписанный сертификат""")
+    choise = input()
+    return choise.strip()
+
+
+def main():
+    servers = load_servers("./servers/servers.yaml")
+    print(f"Сервера загружены: {servers}")
+
+    username = input("Введите логин для SSH:")
+    password = getpass.getpass("Введите пароль для SSH:")
+    
+    for line in servers:
+        ip, port = line.strip().split()
+        hostname = get_hostname(ip, int(port), username, password)
+        if hostname:
+            print(f"{ip}:{port} -> DNS:{hostname}")
+        else:
+            print(f"DNS не получен с {ip}")
+
+    choise = menu()
+    if choise == "1":
+        print("Генерация CA")
+    elif choise == "2":
+        print("Применение серта")
+    else:
+        print("Неверная цифра")
+
+if __name__ == "__main__":
+    main()
