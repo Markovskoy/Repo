91eb35dcc61715c027c15770d26582b45ac4405e
Markovskoy <v_markovskoy@mail.ru>
2025-06-20 00:22:49 +0300

    fix: запятая

--- work_scripts/python_scripts/перевыпуск_ssl_сертов/main.py
+++ work_scripts/python_scripts/перевыпуск_ssl_сертов/main.py
@@ -121,7 +121,7 @@ def generate_csr_on_app1(cluster,username, password):
     commands = [
         "mkdir -p /root/keys",
         f"cd /root/keys && sudo openssl req -new -config openssl_srv.cnf -key private.key -out s{shortname}.ru.csr",
-        f"cd /root/keys && zip dns_{shortname}.zip s{shortname}.ru.csr openssl_srv.cnf"
+        f"cd /root/keys && zip dns_{shortname}.zip s{shortname}.ru.csr openssl_srv.cnf",
         f"sudo cp /root/keys/dns_{shortname}.zip /tmp/dns_{shortname}.zip",
         f"sudo chmod 644 /tmp/dns_{shortname}.zip"
     ]
